7011dc21c1c8c24eb26625977957f215157d37bf
Adam Kwolek <249277@student.pwr.edu.pl>
2019-06-16 15:15:06 +0200

    Upload New File

---
+++ Gracz.hpp
@@ -0,0 +1,38 @@
+//
+//  Gracz.hpp
+//  satki
+//
+//  Created by Adam on 08/06/2019.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+#ifndef Gracz_hpp
+#define Gracz_hpp
+
+#include <iostream>
+#include <string>
+class Plansza;
+
+class Gracz
+{
+    std::string imie;
+    Plansza &plansza;
+    Plansza &plansza2;
+
+
+
+
+
+public:
+Gracz(const std::string  & im, Plansza &p, Plansza &p2);
+    std::string podajImie() const;
+    void graczustaw();
+    void graczustaw2();
+    void graczustaw3();
+
+    void atak();
+    void a2();
+
+};
+
+#endif /* Gracz_hpp */
